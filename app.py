822bb35369c2ac8553c9d8d2d47ef5c00fafc4f6
Sashi Sekhar Singh <118200469+sashi0910@users.noreply.github.com>
2024-07-03 16:34:07 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask,request,jsonify
+import requests
+app = Flask(__name__)
+
+@app.route('/',methods =['POST'])
+def index():
+    data = request.get_json()
+    source_currency = data['queryResult']['parameters']['unit-currency']['currency']
+    amount = data['queryResult']['parameters']['unit-currency']['amount']
+    target_currency = data['queryResult']['parameters']['currency-name']
+
+     # print(source_currency)
+    # print(amount)
+    # print(target_currency)
+
+    cf = fetch_conversion_factor(source_currency,target_currency)
+    final_amount = round(cf * amount,2)
+    response = {
+        'fulfillmentText': "{} {} is {} {}".format(amount, source_currency, final_amount, target_currency)
+    }
+    return jsonify(response)
+def fetch_conversion_factor(source,target):
+    url = "https://v6.exchangerate-api.com/v6/37d8fd143d049dcb5104f781/latest/{}".format(source)
+    #
+    # response = requests.get(url)
+    # response = response.json()
+    # response = response.get(target)
+    response = requests.get(url)
+    data = response.json()
+
+    conversion_rates = data.get("conversion_rates", {})
+
+    target_conversion_rate = conversion_rates.get(target)
+
+
+    return target_conversion_rate
+    # print(response)
+    # return response
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
